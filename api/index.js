0f7e5d16b1f977036d76d42f004b78214ad52cab
sagwa <jerrysagwa0608@gmail.com>
2024-07-23 21:28:04 +0800

    connect to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+
+const { MONGODB_URL, PORT } = process.env;
 
 const app = express();
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    console.log('Connected to MongoDB~');
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-})
+app.listen(PORT, () => {
+  console.log('Server running on port 3000');
+});
