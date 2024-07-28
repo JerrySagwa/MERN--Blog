bd067b0d8040f5000bcfb1f190ae572232809fe7
sagwa <jerrysagwa0608@gmail.com>
2024-07-28 15:00:26 +0800

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import userRoutes from './routes/user.route.js'
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 const { MONGODB_URL, PORT } = process.env;
 
@@ -15,10 +16,9 @@ mongoose
   });
 
 app.listen(PORT, () => {
-  console.log('Server running on port 3000');
+  console.log(`Server running on port ${PORT}`);
 });
-
-app.use(
-  '/api/user',
-  userRoutes
-);
+app.use(express.json()); // 解析 JSON 格式的请求体
+app.use(express.urlencoded({ extended: true })); // 解析 URL 编码的请求体
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
